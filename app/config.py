be9200c78b6c62f3f19f2d1f018c8cad281d3c08
Muhammad Abdullah <printabdullah285@gmail.com>
2025-01-10 18:21:23 +0500

    deleted a simple bug regarding .env file

--- app/config.py
+++ app/config.py
@@ -7,3 +7,13 @@ class Config:
     DEBUG = True
     MONGO_URI = os.getenv("MONGO_URI", "your_default_uri")
     DB_NAME = os.getenv("DB_NAME", "ecomerce")
+
+
+print("MONGO_URI:", os.getenv("MONGO_URI"))
+print("DB_NAME:", os.getenv("DB_NAME"))
+
+
+from pymongo import MongoClient
+
+client = MongoClient(os.getenv("MONGO_URI"))
+print(client.list_database_names())
